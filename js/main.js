13f97bc8ef68e8373221e888b7db18df8355c755
SineYlo <vavshhik01@mail.ru>
2021-10-15 17:41:44 +1000

    written styles for testimonials section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // * ==========> VARIABLES <========== * //
-
-
   // |FYI|: Configuring the Glide.js plugin
-  new Glide('.glide').mount()
-
-
-
+  const swiper = new Glide('.glide', {
+    type: 'carousel',
+    perView: 3.2,
+    gap: 24,
+    autoplay: 3000,
+  }).mount()
 })
